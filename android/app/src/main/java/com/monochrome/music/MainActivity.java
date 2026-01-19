eb7d55beceb4a2dd8325572e7d6696673751bd04
VolodymyrBryzh <volodymyr.bryzh@gmail.com>
2026-01-19 11:21:26 +0200

    feat: Remove the entire music player application.

--- android/app/src/main/java/com/monochrome/music/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.monochrome.music;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.monochrome.music',
-  appName: 'monochrome',
-  webDir: 'dist'
-};
-
-export default config;
